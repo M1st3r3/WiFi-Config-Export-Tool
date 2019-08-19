17e547b3ff215e2f2a47f83053775c5ade7e4c5e
M1st3r3 <46941177+M1st3r3@users.noreply.github.com>
2019-08-19 17:11:43 +0100

    Update Wi-Fi Config Export Tool.ps1

--- Wi-Fi Config Export Tool.ps1
+++ Wi-Fi Config Export Tool.ps1
@@ -7,8 +7,7 @@
 # Version 1.0
 # Created: 10 Jul 2019
 # Updated: N/A
-# Description: Export the Inputed WiFi SSID profile incluing the PSK
-# SHA256 HASH: FF020414974C1E746F9762157F1925A52C9AFCE244C4CF377B51D3D9BE9CDC6E
+# Description: Exports the Inputted WiFi SSID profile including the PSK with multiple options
 # =============================================================================
 # Function Change Log
 # v1.0 - Creation of script
